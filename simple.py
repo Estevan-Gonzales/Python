66cb10780c630620a5fb76662364ca6d18573d38
Estevan Gonzales <egonz94@gmail.com>
2022-07-13 19:05:56 -0500

    changes from whichcraft

--- simple.py
+++ simple.py
@@ -1,27 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from tangled_up_in_unicode import name
+import pandas as pd
 
 app = Flask(__name__)
 
+
+d = pd.read_csv("marvel_cinematic_universe.csv", header = 0)
+
 mcu = {}
-mcu['films'] = [{'name': 'Iron Man'}, {'name': 'Incredible Hulk'}]
-mcu['shows'] = [{'name': 'Wandavisions'}]
+
+mcu['shows'] = []
+mcu['films'] = []
+
+for film in d[d['media'] == "Film"].property:
+    mcu['films'].append({'name': film})
+
+for show in d[d['media'] == 'Series'].property:
+    mcu['shows'].append({'name': show})
+
 
 @app.route("/")
-def hello_world():
-  return '''
-    <!DOCTYPE html>
-    <html>
-      <body>
-        <h1>Welcome to the MCU!</h1>
-        <h2>Feature Films</h2>
-            <ul>
-            <li><a href = '/properties/films'>Films</a></li>
-            <li><a href = '/properties/shows'>TV Shows</a></li>
-            </ul>
-      </body>
-    </html>
-    '''
+def index():
+  return render_template("index.html")
 
 
 
@@ -30,16 +30,16 @@ def properties(property_type):
   html = f'<h1>List of {property_type}</h1>'
   html += '<ul>'
   for idx, property in enumerate(mcu[property_type]):
-    html += f'''<li><a href = '/properties/{property_type}/{idx}'>{property["name"]}</a></li>'''
+    html += f'''<li><a href = '/properties/{property_type}/{idx}'>{property['name']}</a></li>'''
   html += '</ul>'
   return html
 
 @app.route('/properties/<property_type>/<int:property_id>')
 def property(property_type, property_id):
   property = mcu[property_type][property_id]
-  html = f'''<h1>{property["name"]}</h1>'''
+  html = f'''<h1>{property['name']}</h1>'''
 
   return html
 #To run this:
-#~export FLASK_APP = {{file_name.py}}~
+#~export FLASK_APP={{file_name.py}}~
 #~flask run~
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-x = 1
+import pandas as pd
 
-print(x)
+d = pd.read_csv("marvel_cinematic_universe.csv", header = 0)
+
+mcu = {}
+
+mcu['shows'] = []
+mcu['films'] = []
+
+for film in d[d['media'] == "Film"].property:
+    mcu['films'].append({'name': film})
+
+for show in d[d['media'] == 'Series'].property:
+    mcu['shows'].append({'name': show})
+
+print(mcu)
